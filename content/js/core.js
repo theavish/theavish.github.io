3fe7541316234c0d71752dc477e2f9e97220fdb5
Avi Samloff <avi.samloff@gmail.com>
2017-08-18 00:02:17 -0500

    Fix mobile class addition

--- content/js/core.js
+++ content/js/core.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (mobilecheck()) {
-        document.getElementsByClassName('content').className += " mobile";
+        document.getElementsByClassName('content')[0].className.push(" mobile");
     }
 });
