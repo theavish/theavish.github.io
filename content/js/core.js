dfdfef5428990ffd91329f7956cb31de5fc61f99
Avi Samloff <avi.samloff@gmail.com>
2017-08-18 00:03:28 -0500

    Remove push in favor of concatenation

--- content/js/core.js
+++ content/js/core.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (mobilecheck()) {
-        document.getElementsByClassName('content')[0].className.push(" mobile");
+        document.getElementsByClassName('content')[0].className += " mobile";
     }
 });
